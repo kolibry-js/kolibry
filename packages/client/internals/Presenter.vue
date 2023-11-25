cd5ccb7e56f81ebc7ec615b56f3f7a01447384c8
nyxb <contact@nyxb.email>
2023-11-25 16:08:59 +0100

    ✨ feat(client): add new Presenter.vue component for slide navigation
    This commit introduces a new Vue component, Presenter.vue, which provides a comprehensive interface for navigating and controlling a slide presentation. The component includes features such as a timer, navigation controls, slide previews, note editing, and more, all tailored for a presenter's needs during a presentation. The addition of this component aims to enhance the user experience for presenters by offering a centralized and interactive way to manage their slides and presentation flow.

---
+++ packages/client/internals/Presenter.vue
@@ -0,0 +1,286 @@
+<script setup lang="ts">
+import { useHead } from '@unhead/vue'
+import { computed, onMounted, reactive, ref, shallowRef, watch } from 'vue'
+import { useMouse, useWindowFocus } from '@vueuse/core'
+import { clicks, clicksTotal, currentPage, currentRoute, hasNext, nextRoute, total, useSwipeControls } from '../logic/nav'
+import { decreasePresenterFontSize, increasePresenterFontSize, presenterLayout, presenterNotesFontSize, showEditor, showOverview, showPresenterCursor } from '../state'
+import { configs, themeVars } from '../env'
+import { sharedState } from '../state/shared'
+import { registerShortcuts } from '../logic/shortcuts'
+import { getSlideClass } from '../utils'
+import { useTimer } from '../logic/utils'
+import { isDrawing } from '../logic/drawings'
+import SlideContainer from './SlideContainer.vue'
+import NavControls from './NavControls.vue'
+import SlidesOverview from './SlidesOverview.vue'
+import NoteEditor from './NoteEditor.vue'
+import NoteStatic from './NoteStatic.vue'
+import Goto from './Goto.vue'
+import SlidesShow from './SlidesShow.vue'
+import SlideWrapper from './SlideWrapper'
+import DrawingControls from './DrawingControls.vue'
+
+const main = ref<HTMLDivElement>()
+
+registerShortcuts()
+useSwipeControls(main)
+
+const slideTitle = configs.titleTemplate.replace('%s', configs.title || 'EnigmaSlidev')
+useHead({
+  title: `Presenter - ${slideTitle}`,
+})
+
+const notesEditing = ref(false)
+
+const { timer, resetTimer } = useTimer()
+
+const nextTabElements = ref([])
+const nextSlide = computed(() => {
+  if (clicks.value < clicksTotal.value) {
+    return {
+      route: currentRoute.value,
+      clicks: clicks.value + 1,
+    }
+  }
+  else {
+    if (hasNext.value) {
+      return {
+        route: nextRoute.value,
+        clicks: 0,
+      }
+    }
+    else {
+      return null
+    }
+  }
+})
+
+const Editor = shallowRef<any>()
+if (__DEV__ && __ENIGMASLIDEV_FEATURE_EDITOR__)
+  import('./Editor.vue').then(v => Editor.value = v.default)
+
+// sync presenter cursor
+onMounted(() => {
+  const slidesContainer = main.value!.querySelector('#slide-content')!
+  const mouse = reactive(useMouse())
+  const focus = useWindowFocus()
+
+  watch(
+    () => {
+      if (!focus.value || isDrawing.value || !showPresenterCursor.value)
+        return undefined
+
+      const rect = slidesContainer.getBoundingClientRect()
+      const x = (mouse.x - rect.left) / rect.width * 100
+      const y = (mouse.y - rect.top) / rect.height * 100
+
+      if (x < 0 || x > 100 || y < 0 || y > 100)
+        return undefined
+
+      return { x, y }
+    },
+    (pos) => {
+      sharedState.cursor = pos
+    },
+  )
+})
+</script>
+
+<template>
+  <div class="bg-main h-full enigmaslidev-presenter">
+    <div class="grid-container" :class="`layout${presenterLayout}`">
+      <div class="grid-section top flex">
+        <img src="../assets/logo-title-horizontal.png" class="ml-2 my-auto h-10 py-1 lg:h-14 lg:py-2" style="height: 3.5rem;" alt="EnigmaSlidev logo">
+        <div class="flex-auto" />
+        <div
+          class="timer-btn my-auto relative w-22px h-22px cursor-pointer text-lg"
+          opacity="50 hover:100"
+          @click="resetTimer"
+        >
+          <carbon:time class="absolute" />
+          <carbon:renew class="absolute opacity-0" />
+        </div>
+        <div class="text-2xl pl-2 pr-6 my-auto tabular-nums">
+          {{ timer }}
+        </div>
+      </div>
+      <div ref="main" class="relative grid-section main flex flex-col p-2 lg:p-4" :style="themeVars">
+        <SlideContainer
+          key="main"
+          class="h-full w-full"
+        >
+          <template #default>
+            <SlidesShow render-context="presenter" />
+          </template>
+        </SlideContainer>
+        <div class="context">
+          current
+        </div>
+      </div>
+      <div class="relative grid-section next flex flex-col p-2 lg:p-4" :style="themeVars">
+        <SlideContainer
+          v-if="nextSlide"
+          key="next"
+          class="h-full w-full"
+        >
+          <SlideWrapper
+            :is="nextSlide.route?.component as any"
+            v-model:clicks-elements="nextTabElements"
+            :clicks="nextSlide.clicks"
+            :clicks-disabled="false"
+            :class="getSlideClass(nextSlide.route)"
+            :route="nextSlide.route"
+            render-context="previewNext"
+          />
+        </SlideContainer>
+        <div class="context">
+          next
+        </div>
+      </div>
+      <!-- Notes -->
+      <div v-if="__DEV__ && __ENIGMASLIDEV_FEATURE_EDITOR__ && Editor && showEditor" class="grid-section note of-auto">
+        <Editor />
+      </div>
+      <div v-else class="grid-section note grid grid-rows-[1fr_min-content] overflow-hidden">
+        <NoteEditor
+          v-if="__DEV__"
+          class="w-full max-w-full h-full overflow-auto p-2 lg:p-4"
+          :editing="notesEditing"
+          :style="{ fontSize: `${presenterNotesFontSize}em` }"
+        />
+        <NoteStatic
+          v-else
+          class="w-full max-w-full h-full overflow-auto p-2 lg:p-4"
+          :style="{ fontSize: `${presenterNotesFontSize}em` }"
+        />
+        <div class="border-t border-main py-1 px-2 text-sm">
+          <button class="enigmaslidev-icon-btn" @click="increasePresenterFontSize">
+            <HiddenText text="Increase font size" />
+            <carbon:zoom-in />
+          </button>
+          <button class="enigmaslidev-icon-btn" @click="decreasePresenterFontSize">
+            <HiddenText text="Decrease font size" />
+            <carbon:zoom-out />
+          </button>
+          <button
+            v-if="__DEV__"
+            class="enigmaslidev-icon-btn" @click="notesEditing = !notesEditing"
+          >
+            <HiddenText text="Edit Notes" />
+            <carbon:edit />
+          </button>
+        </div>
+      </div>
+      <div class="grid-section bottom">
+        <NavControls :persist="true" />
+      </div>
+      <DrawingControls v-if="__ENIGMASLIDEV_FEATURE_DRAWINGS__" />
+    </div>
+    <div class="progress-bar">
+      <div
+        class="progress h-2px bg-primary transition-all"
+        :style="{ width: `${(currentPage - 1) / (total - 1) * 100}%` }"
+      />
+    </div>
+  </div>
+  <Goto />
+  <SlidesOverview v-model="showOverview" />
+</template>
+
+<style lang="postcss" scoped>
+.enigmaslidev-presenter {
+  --enigmaslidev-controls-foreground: current;
+}
+
+.timer-btn:hover {
+  & > :first-child {
+    @apply opacity-0;
+  }
+  & > :last-child {
+    @apply opacity-100;
+  }
+}
+
+.section-title {
+  @apply px-4 py-2 text-xl;
+}
+
+.grid-container {
+  @apply h-full w-full bg-gray-400 bg-opacity-15;
+  display: grid;
+  gap: 1px 1px;
+}
+
+.grid-container.layout1 {
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: min-content 2fr 1fr min-content;
+  grid-template-areas:
+    "top top"
+    "main main"
+    "note next"
+    "bottom bottom";
+}
+
+.grid-container.layout2 {
+  grid-template-columns: 3fr 2fr;
+  grid-template-rows: min-content 2fr 1fr min-content;
+  grid-template-areas:
+    "top top"
+    "note main"
+    "note next"
+    "bottom bottom";
+}
+
+@media (max-aspect-ratio: 3/5) {
+  .grid-container.layout1 {
+    grid-template-columns: 1fr;
+    grid-template-rows: min-content 1fr 1fr 1fr min-content;
+    grid-template-areas:
+      "top"
+      "main"
+      "note"
+      "next"
+      "bottom";
+  }
+}
+
+@media (min-aspect-ratio: 1/1) {
+  .grid-container.layout1 {
+    grid-template-columns: 1fr 1.1fr 0.9fr;
+    grid-template-rows: min-content 1fr 2fr min-content;
+    grid-template-areas:
+      "top top top"
+      "main main next"
+      "main main note"
+      "bottom bottom bottom";
+  }
+}
+
+.progress-bar {
+  @apply fixed left-0 right-0 bottom-0;
+}
+
+.grid-section {
+  @apply bg-main;
+
+  &.top {
+    grid-area: top;
+  }
+  &.main {
+    grid-area: main;
+  }
+  &.next {
+    grid-area: next;
+  }
+  &.note {
+    grid-area: note;
+  }
+  &.bottom {
+    grid-area: bottom;
+  }
+}
+
+.context {
+  @apply absolute top-0 left-0 px-1 text-xs bg-gray-400 bg-opacity-50 opacity-75 rounded-br-md;
+}
+</style>
