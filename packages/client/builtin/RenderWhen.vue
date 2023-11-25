3b651845ca307e538a46291c09af222a55e8f4c8
nyxb <contact@nyxb.email>
2023-11-25 15:01:55 +0100

    ✨ feat(RenderWhen.vue): add new RenderWhen component for conditional rendering
    This component allows for rendering content based on the current rendering context, improving the flexibility and reusability of UI components within the application.

---
+++ packages/client/builtin/RenderWhen.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { computed, inject } from 'vue'
+import type { RenderContext } from '@enigmaslidev/types'
+
+import { injectionRenderContext } from '../constants'
+
+type Context = 'main' | RenderContext
+
+const props = defineProps<{
+  context: Context | Context[]
+}>()
+const { context } = props
+
+const currentContext = inject(injectionRenderContext)
+
+const shouldRender = computed(() => Array.isArray(context) ? context.some(contextMatch) : contextMatch(context))
+
+function contextMatch(context: Context) {
+  if (context === currentContext?.value)
+    return true
+  if (context === 'main' && (currentContext?.value === 'slide' || currentContext?.value === 'presenter'))
+    return true
+  return false
+}
+</script>
+
+<template>
+  <slot v-if="shouldRender" />
+</template>
