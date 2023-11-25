1271db093e02b2430b90f588c4850117620a1ac4
nyxb <contact@nyxb.email>
2023-11-25 14:48:15 +0100

    ✨ feat(client/setup): add main setup with Motion and Starport plugins
    Adds a new setup file for the client package, integrating VueUse Motion and Vue Starport for enhanced UI animations and transitions. The setup also includes a viewport height adjustment for mobile devices to disable navbar scroll issues.

---
+++ packages/client/setup/main.ts
@@ -0,0 +1,24 @@
+/* __imports__ */
+
+import type { AppContext } from '@enigmaslidev/types'
+import { MotionPlugin } from '@vueuse/motion'
+import StarportPlugin from 'vue-starport'
+
+export default function setupMain(context: AppContext) {
+  function setMaxHeight() {
+    // disable the mobile navbar scroll
+    // see https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
+    document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`)
+  }
+  setMaxHeight()
+  window.addEventListener('resize', setMaxHeight)
+
+  context.app.use(MotionPlugin)
+  context.app.use(StarportPlugin({ keepAlive: true }))
+
+  // @ts-expect-error inject in runtime
+  // eslint-disable-next-line unused-imports/no-unused-vars
+  const injection_arg = context
+
+  /* __injections__ */
+}
