ea969eec3eaef210354178d450a59aef45176cce
nyxb <contact@nyxb.email>
2023-11-25 15:02:03 +0100

    ✨ feat(builtin): add SlideCurrentNo.vue component to display current page number

---
+++ packages/client/builtin/SlideCurrentNo.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { inject } from 'vue'
+import { injectionCurrentPage } from '../constants'
+
+const $page = inject(injectionCurrentPage)
+</script>
+
+<template>
+  <span>{{ $page }}</span>
+</template>
