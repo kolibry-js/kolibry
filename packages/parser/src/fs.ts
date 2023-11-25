f73ccfbadc6074f5d8de890f6cd6dd54722fba92
nyxb <contact@nyxb.email>
2023-11-25 02:05:47 +0100

    ✨ feat(parser/fs.ts): add file system utilities for parser package
    This new module provides async file system operations tailored for the parser package, including loading and saving markdown content, handling preparser extensions, and managing slide data with theme metadata support.

---
+++ packages/parser/src/fs.ts
@@ -0,0 +1,105 @@
+import { promises as fs } from 'node:fs'
+import { dirname, resolve } from 'node:path'
+import type { PreparserExtensionLoader, SlideInfo, SlideInfoWithPath, EnigmaSlidevMarkdown, EnigmaSlidevPreparserExtension, EnigmaSlidevThemeMeta } from '@enigmaslidev/types'
+import { detectFeatures, mergeFeatureFlags, parse, stringify, stringifySlide } from './core'
+
+export * from './core'
+
+let preparserExtensionLoader: PreparserExtensionLoader | null = null
+
+export function injectPreparserExtensionLoader(fn: PreparserExtensionLoader) {
+  preparserExtensionLoader = fn
+}
+
+export async function load(filepath: string, themeMeta?: EnigmaSlidevThemeMeta, content?: string) {
+  const dir = dirname(filepath)
+  const markdown = content ?? await fs.readFile(filepath, 'utf-8')
+
+  const preparserExtensions: EnigmaSlidevPreparserExtension[] = []
+  const data = await parse(markdown, filepath, themeMeta, [], async (headmatter, exts: EnigmaSlidevPreparserExtension[], filepath: string | undefined) => {
+    preparserExtensions.splice(
+      0,
+      preparserExtensions.length,
+      ...exts,
+      ...preparserExtensionLoader ? await preparserExtensionLoader(headmatter, filepath) : [],
+    )
+    return preparserExtensions
+  })
+
+  const entries = new Set([
+    filepath,
+  ])
+
+  for (let iSlide = 0; iSlide < data.slides.length;) {
+    const baseSlide = data.slides[iSlide]
+    if (!baseSlide.frontmatter.src) {
+      iSlide++
+      continue
+    }
+
+    data.slides.splice(iSlide, 1)
+
+    if (baseSlide.frontmatter.hide)
+      continue
+
+    const srcExpression = baseSlide.frontmatter.src
+    let path
+    if (srcExpression.startsWith('/'))
+      path = resolve(dir, srcExpression.substring(1))
+    else if (baseSlide.source?.filepath)
+      path = resolve(dirname(baseSlide.source.filepath), srcExpression)
+    else
+      path = resolve(dir, srcExpression)
+
+    const raw = await fs.readFile(path, 'utf-8')
+    const subSlides = await parse(raw, path, themeMeta, preparserExtensions)
+
+    for (const [offset, subSlide] of subSlides.slides.entries()) {
+      const slide: SlideInfo = { ...baseSlide }
+
+      slide.source = {
+        filepath: path,
+        ...subSlide,
+      }
+
+      if (offset === 0 && !baseSlide.frontmatter.srcSequence) {
+        slide.inline = { ...baseSlide }
+        delete slide.inline.frontmatter.src
+        Object.assign(slide, slide.source, { raw: null })
+      }
+      else {
+        Object.assign(slide, slide.source)
+      }
+
+      const baseSlideFrontMatterWithoutSrc = { ...baseSlide.frontmatter }
+      delete baseSlideFrontMatterWithoutSrc.src
+
+      slide.frontmatter = {
+        ...subSlide.frontmatter,
+        ...baseSlideFrontMatterWithoutSrc,
+        srcSequence: `${baseSlide.frontmatter.srcSequence ? `${baseSlide.frontmatter.srcSequence},` : ''}${srcExpression}`,
+      }
+
+      data.features = mergeFeatureFlags(data.features, detectFeatures(raw))
+      entries.add(path)
+      data.slides.splice(iSlide + offset, 0, slide)
+    }
+  }
+  // re-index slides
+  for (let iSlide = 0; iSlide < data.slides.length; iSlide++)
+    data.slides[iSlide].index = iSlide === 0 ? 0 : 1 + data.slides[iSlide - 1].index
+
+  data.entries = Array.from(entries)
+
+  return data
+}
+
+export async function save(data: EnigmaSlidevMarkdown, filepath?: string) {
+  filepath = filepath || data.filepath!
+
+  await fs.writeFile(filepath, stringify(data), 'utf-8')
+}
+
+export async function saveExternalSlide(slide: SlideInfoWithPath) {
+  await fs.writeFile(slide.filepath, stringifySlide(slide), 'utf-8')
+}
