d41f3b796edde01f9f7bbe72e4d0ed89cb867181
nyxb <contact@nyxb.email>
2023-11-25 14:47:50 +0100

    ✨ feat(client/routes.ts): add new Vue router configuration and routes
    This commit introduces a new routing configuration for the Vue client application, including the setup of the Vue router with hash and history modes, route definitions for 'play', 'print', and 'presenter' views, as well as redirects and password protection for certain routes. The commit also extends the RouteMeta interface to include metadata for slide information and private fields. This change is essential for enabling navigation within the Vue application and ensuring that the correct components are rendered for each route.

---
+++ packages/client/routes.ts
@@ -0,0 +1,107 @@
+import type { RouteLocationNormalized, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
+import type { TransitionGroupProps } from 'vue'
+import Play from './internals/Play.vue'
+import Print from './internals/Print.vue'
+
+// @ts-expect-error missing types
+import _rawRoutes, { redirects } from '/@enigmaslidev/routes'
+
+// @ts-expect-error missing types
+import _configs from '/@enigmaslidev/configs'
+
+export const rawRoutes = _rawRoutes as RouteRecordRaw[]
+
+export const routes: RouteRecordRaw[] = [
+  {
+    name: 'play',
+    path: '/',
+    component: Play,
+    children: [
+      ...rawRoutes,
+      ...redirects,
+    ],
+  },
+  { name: 'print', path: '/print', component: Print },
+  { path: '', redirect: { path: '/1' } },
+  { path: '/:pathMatch(.*)', redirect: { path: '/1' } },
+]
+
+if (__ENIGMASLIDEV_FEATURE_PRESENTER__) {
+  function passwordGuard(to: RouteLocationNormalized) {
+    if (!_configs.remote || _configs.remote === to.query.password)
+      return true
+    if (_configs.remote && to.query.password === undefined) {
+      // eslint-disable-next-line no-alert
+      const password = prompt('Enter password')
+      if (_configs.remote === password)
+        return true
+    }
+    if (to.params.no)
+      return { path: `/${to.params.no}` }
+    return { path: '' }
+  }
+  routes.push({ path: '/presenter/print', component: () => import('./internals/PresenterPrint.vue') })
+  if (__ENIGMASLIDEV_HAS_SERVER__) {
+    routes.push({
+      name: 'entry',
+      path: '/entry',
+      component: () => import('./internals/EntrySelect.vue'),
+    })
+    routes.push({
+      name: 'notes',
+      path: '/notes',
+      component: () => import('./internals/NotesView.vue'),
+      beforeEnter: passwordGuard,
+    })
+  }
+  routes.push({
+    name: 'presenter',
+    path: '/presenter/:no',
+    component: () => import('./internals/Presenter.vue'),
+    beforeEnter: passwordGuard,
+  })
+  routes.push({
+    path: '/presenter',
+    redirect: { path: '/presenter/1' },
+  })
+}
+
+export const router = createRouter({
+  history: __ENIGMASLIDEV_HASH_ROUTE__
+    ? createWebHashHistory(import.meta.env.BASE_URL)
+    : createWebHistory(import.meta.env.BASE_URL),
+  routes,
+})
+
+declare module 'vue-router' {
+  interface RouteMeta {
+    // inherited from frontmatter
+    layout: string
+    name?: string
+    class?: string
+    clicks?: number
+    transition?: string | TransitionGroupProps | undefined
+    preload?: boolean
+
+    // slide info
+    slide?: {
+      start: number
+      end: number
+      note?: string
+      noteHTML?: string
+      id: number
+      no: number
+      filepath: string
+      title?: string
+      level?: number
+      raw: string
+      content: string
+      frontmatter: Record<string, any>
+    }
+
+    // private fields
+    __clicksElements: HTMLElement[]
+    __preloaded?: boolean
+  }
+}
