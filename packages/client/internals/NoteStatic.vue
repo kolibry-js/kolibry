c595f21f47143909ce986d0a138b715936172f86
nyxb <contact@nyxb.email>
2023-11-25 16:08:34 +0100

    ✨ feat(client): add NoteStatic.vue component for static note display

---
+++ packages/client/internals/NoteStatic.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { currentRoute } from '../logic/nav'
+import NoteDisplay from './NoteDisplay.vue'
+
+const props = defineProps<{
+  class?: string
+}>()
+
+const note = computed(() => currentRoute.value?.meta?.slide?.note)
+const noteHtml = computed(() => currentRoute.value?.meta?.slide?.noteHTML)
+</script>
+
+<template>
+  <NoteDisplay
+    :class="props.class"
+    :note="note"
+    :note-html="noteHtml"
+  />
+</template>
