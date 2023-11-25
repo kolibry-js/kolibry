5458d250b3553f0923069176f60e22e02cc0d0c9
nyxb <contact@nyxb.email>
2023-11-25 16:06:23 +0100

    ✨ feat(client): add new Controls.vue component for UI controls
    This component introduces UI controls such as slide overview, navigation, and conditional camera and recording dialogues based on feature flags.

---
+++ packages/client/internals/Controls.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { shallowRef } from 'vue'
+import { showInfoDialog, showOverview, showRecordingDialog } from '../state'
+import { configs } from '../env'
+import SlidesOverview from './SlidesOverview.vue'
+import InfoDialog from './InfoDialog.vue'
+import Goto from './Goto.vue'
+
+const WebCamera = shallowRef<any>()
+const RecordingDialog = shallowRef<any>()
+if (__ENIGMASLIDEV_FEATURE_RECORD__) {
+  import('./WebCamera.vue').then(v => WebCamera.value = v.default)
+  import('./RecordingDialog.vue').then(v => RecordingDialog.value = v.default)
+}
+</script>
+
+<template>
+  <SlidesOverview v-model="showOverview" />
+  <Goto />
+  <WebCamera v-if="WebCamera" />
+  <RecordingDialog v-if="RecordingDialog" v-model="showRecordingDialog" />
+  <InfoDialog v-if="configs.info" v-model="showInfoDialog" />
+</template>
