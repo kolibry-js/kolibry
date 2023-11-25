7e0a6dd6a7ce01c62a0421fed95c26857f2146cb
nyxb <contact@nyxb.email>
2023-11-25 16:09:06 +0100

    ✨ feat(PresenterMouse.vue): add new Vue component for mouse cursor presentation
    This component tracks and displays the mouse cursor position using a shared state.

---
+++ packages/client/internals/PresenterMouse.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { sharedState } from '../state/shared'
+</script>
+
+<template>
+  <div
+    v-if="sharedState.cursor"
+    class="absolute top-0 left-0 right-0 bottom-0 pointer-events-none text-xl"
+  >
+    <ph-cursor-fill
+      class="absolute"
+      :style="{ left: `${sharedState.cursor.x}%`, top: `${sharedState.cursor.y}%` }"
+    />
+  </div>
+</template>
