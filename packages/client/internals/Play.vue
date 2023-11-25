edc54ff0fbb05461901f39d96c9f09c57c5ca4ca
nyxb <contact@nyxb.email>
2023-11-25 16:08:50 +0100

    ✨ feat(client): add new Play.vue component for enhanced navigation and control
    This new Vue component introduces a more interactive and user-friendly interface for navigating slides, including support for keyboard shortcuts, swipe controls, and conditional rendering of editor and drawing controls based on feature flags and device orientation.

---
+++ packages/client/internals/Play.vue
@@ -0,0 +1,77 @@
+<script setup lang="ts">
+import { computed, ref, shallowRef } from 'vue'
+import { isScreenVertical, showEditor, slideScale, windowSize } from '../state'
+import { isEmbedded, isPrintMode, next, prev, useSwipeControls } from '../logic/nav'
+import { isDrawing } from '../logic/drawings'
+import { registerShortcuts } from '../logic/shortcuts'
+import { configs, themeVars } from '../env'
+import Controls from './Controls.vue'
+import SlideContainer from './SlideContainer.vue'
+import NavControls from './NavControls.vue'
+import SlidesShow from './SlidesShow.vue'
+import PrintStyle from './PrintStyle.vue'
+
+registerShortcuts()
+
+const root = ref<HTMLDivElement>()
+function onClick(e: MouseEvent) {
+  if (showEditor.value)
+    return
+
+  if ((e.target as HTMLElement)?.id === 'slide-container') {
+    // click right to next, left to previous
+    if ((e.screenX / window.innerWidth) > 0.6)
+      next()
+    else
+      prev()
+  }
+}
+
+useSwipeControls(root)
+
+const persistNav = computed(() => isScreenVertical.value || showEditor.value)
+
+const Editor = shallowRef<any>()
+if (__DEV__ && __ENIGMASLIDEV_FEATURE_EDITOR__)
+  import('./Editor.vue').then(v => Editor.value = v.default)
+
+const DrawingControls = shallowRef<any>()
+if (__ENIGMASLIDEV_FEATURE_DRAWINGS__)
+  import('./DrawingControls.vue').then(v => DrawingControls.value = v.default)
+</script>
+
+<template>
+  <PrintStyle v-if="isPrintMode" />
+  <div id="page-root" ref="root" class="grid grid-cols-[1fr_max-content]" :style="themeVars">
+    <SlideContainer
+      class="w-full h-full"
+      :style="{ background: 'var(--enigmaslidev-slide-container-background, black)' }"
+      :width="isPrintMode ? windowSize.width.value : undefined"
+      :scale="slideScale"
+      @pointerdown="onClick"
+    >
+      <template #default>
+        <SlidesShow render-context="slide" />
+      </template>
+      <template #controls>
+        <div
+          class="absolute bottom-0 left-0 transition duration-300 opacity-0 hover:opacity-100"
+          :class="[
+            persistNav ? 'opacity-100 right-0' : 'opacity-0 p-2',
+            isDrawing ? 'pointer-events-none' : '',
+          ]"
+        >
+          <NavControls class="m-auto" :persist="persistNav" />
+        </div>
+        <template v-if="__ENIGMASLIDEV_FEATURE_DRAWINGS__ && !configs.drawings.presenterOnly && !isEmbedded && DrawingControls">
+          <DrawingControls class="ml-0" />
+        </template>
+      </template>
+    </SlideContainer>
+
+    <template v-if="__DEV__ && __ENIGMASLIDEV_FEATURE_EDITOR__ && Editor && showEditor">
+      <Editor :resize="true" />
+    </template>
+  </div>
+  <Controls />
+</template>
