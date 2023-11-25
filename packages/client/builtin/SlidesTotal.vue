7b9f2c50595beb8f6cb1211c419c2faec6fd7975
nyxb <contact@nyxb.email>
2023-11-25 15:02:12 +0100

    ✨ feat(SlidesTotal.vue): add new Vue component to display total slides count
    This new component will inject the EnigmaSlidev context and use it to display the total number of slides in the presentation.

---
+++ packages/client/builtin/SlidesTotal.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { inject } from 'vue'
+import { injectionEnigmaSlidevContext } from '../constants'
+
+const $enigmaslidev = inject(injectionEnigmaSlidevContext)
+</script>
+
+<template>
+  <span>{{ $enigmaslidev.nav.total }}</span>
+</template>
