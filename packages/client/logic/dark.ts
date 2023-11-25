10b8de68c69327edd1f6f61fd624f4db6a5cda9f
nyxb <contact@nyxb.email>
2023-11-25 14:45:25 +0100

    ✨ feat(client-logic): add dark mode toggle functionality
    This commit introduces a new dark mode logic for the client package, allowing users to toggle between dark and light themes. It uses Vue's reactivity system and the VueUse library to sync the theme with the user's preferred settings and local storage. This feature enhances the user experience by providing a customizable interface that respects system preferences and persists across sessions.

---
+++ packages/client/logic/dark.ts
@@ -0,0 +1,38 @@
+import { isClient, useLocalStorage, usePreferredDark, useToggle } from '@vueuse/core'
+import { computed, watch } from 'vue'
+import { configs } from '../env'
+
+const preferredDark = usePreferredDark()
+const store = useLocalStorage('enigmaslidev-color-schema', 'auto')
+
+export const isColorSchemaConfigured = computed(() => configs.colorSchema !== 'auto')
+export const isDark = computed<boolean>({
+  get() {
+    if (isColorSchemaConfigured.value)
+      return configs.colorSchema === 'dark'
+    return store.value === 'auto'
+      ? preferredDark.value
+      : store.value === 'dark'
+  },
+  set(v) {
+    if (isColorSchemaConfigured.value)
+      return
+    store.value = v === preferredDark.value
+      ? 'auto'
+      : v ? 'dark' : 'light'
+  },
+})
+
+export const toggleDark = useToggle(isDark)
+
+if (isClient) {
+  watch(
+    isDark,
+    (v) => {
+      const html = document.querySelector('html')!
+      html.classList.toggle('dark', v)
+      html.classList.toggle('light', !v)
+    },
+    { immediate: true },
+  )
+}
