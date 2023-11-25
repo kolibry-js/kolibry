bf96626b6e37815ff3fc2b1edc400e0ec4d08353
nyxb <contact@nyxb.email>
2023-11-25 16:08:01 +0100

    ✨ feat(MenuButton.vue): add new MenuButton component for dropdown functionality

---
+++ packages/client/internals/MenuButton.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { onClickOutside, useVModel } from '@vueuse/core'
+import { ref } from 'vue'
+
+const props = defineProps({
+  modelValue: {
+    default: false,
+  },
+  disabled: {
+    default: false,
+  },
+})
+
+const emit = defineEmits<{
+  (e: any): void
+}>()
+const value = useVModel(props, 'modelValue', emit, { passive: true })
+const el = ref<HTMLDivElement>()
+
+onClickOutside(el, () => {
+  value.value = false
+})
+</script>
+
+<template>
+  <div ref="el" class="flex relative">
+    <button :class="{ disabled }" @click="value = !value">
+      <slot name="button" :class="{ disabled }" />
+    </button>
+    <KeepAlive>
+      <div
+        v-if="value"
+        class="rounded-md bg-main shadow absolute bottom-10 left-0 z-20"
+        dark:border="~ main"
+      >
+        <slot name="menu" />
+      </div>
+    </KeepAlive>
+  </div>
+</template>
