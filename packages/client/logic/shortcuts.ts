23591cea5603e86b93579433e52f941f1ed88021
nyxb <contact@nyxb.email>
2023-11-25 14:46:44 +0100

    ✨ feat(client/logic): add keyboard shortcuts handling logic
    Add a new shortcuts handling module to the client logic, enabling the use of keyboard shortcuts within the application. This includes the ability to define shortcuts, handle auto-repeat functionality, and toggle fullscreen mode with a shortcut. The implementation leverages Vue's reactivity system and the `@vueuse/core` library for a clean and reactive approach to shortcut management.

---
+++ packages/client/logic/shortcuts.ts
@@ -0,0 +1,57 @@
+import type { Fn, KeyFilter } from '@vueuse/core'
+import { onKeyStroke } from '@vueuse/core'
+import { and, not } from '@vueuse/math'
+import type { Ref } from 'vue'
+import { watch } from 'vue'
+import type { ShortcutOptions } from '@enigmaslidev/types'
+import { fullscreen, isInputting, isOnFocus, magicKeys, shortcutsEnabled } from '../state'
+import setupShortcuts from '../setup/shortcuts'
+
+const _shortcut = and(not(isInputting), not(isOnFocus), shortcutsEnabled)
+
+export function shortcut(key: string | Ref<boolean>, fn: Fn, autoRepeat = false) {
+  if (typeof key === 'string')
+    key = magicKeys[key]
+
+  const source = and(key, _shortcut)
+  let count = 0
+  let timer: any
+  const trigger = () => {
+    clearTimeout(timer)
+    if (!source.value) {
+      count = 0
+      return
+    }
+    if (autoRepeat) {
+      timer = setTimeout(trigger, Math.max(1000 - count * 250, 150))
+      count++
+    }
+    fn()
+  }
+
+  return watch(source, trigger, { flush: 'sync' })
+}
+
+export function strokeShortcut(key: KeyFilter, fn: Fn) {
+  return onKeyStroke(key, (ev) => {
+    if (!_shortcut.value)
+      return
+    if (!ev.repeat)
+      fn()
+  })
+}
+
+export function registerShortcuts() {
+  const allShortcuts = setupShortcuts()
+
+  const shortcuts = new Map<string | Ref<boolean>, ShortcutOptions>(
+    allShortcuts.map((options: ShortcutOptions) => [options.key, options]),
+  )
+
+  shortcuts.forEach((options) => {
+    if (options.fn)
+      shortcut(options.key, options.fn, options.autoRepeat)
+  })
+
+  strokeShortcut('f', () => fullscreen.toggle())
+}
