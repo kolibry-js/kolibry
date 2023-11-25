ab898ec084a8660c42c8b29994aefa94c8b0b577
nyxb <contact@nyxb.email>
2023-11-25 16:09:45 +0100

    ✨ feat(client): add PrintSlideClick component for print slide functionality

---
+++ packages/client/internals/PrintSlideClick.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+import type { RouteRecordRaw } from 'vue-router'
+import { computed, provide, reactive, shallowRef } from 'vue'
+import { useVModel } from '@vueuse/core'
+import { useNavClicks } from '../composables/useNavClicks'
+import { injectionEnigmaSlidevContext } from '../constants'
+import { isClicksDisabled } from '../logic/nav'
+import { configs, slideHeight, slideWidth } from '../env'
+import { getSlideClass } from '../utils'
+import type { EnigmaSlidevContextNav } from '../modules/context'
+import SlideWrapper from './SlideWrapper'
+
+// @ts-expect-error virtual module
+import GlobalTop from '/@enigmaslidev/global-components/top'
+
+// @ts-expect-error virtual module
+import GlobalBottom from '/@enigmaslidev/global-components/bottom'
+
+const props = defineProps<{
+  clicks: number
+  clicksElements?: HTMLElement[]
+  nav: EnigmaSlidevContextNav
+  route: RouteRecordRaw
+}>()
+
+const emit = defineEmits(['update:clicksElements'])
+
+const clicksElements = useVModel(props, 'clicksElements', emit)
+
+const style = computed(() => ({
+  height: `${slideHeight}px`,
+  width: `${slideWidth}px`,
+}))
+
+const DrawingPreview = shallowRef<any>()
+if (__ENIGMASLIDEV_FEATURE_DRAWINGS__ || __ENIGMASLIDEV_FEATURE_DRAWINGS_PERSIST__)
+  import('./DrawingPreview.vue').then(v => (DrawingPreview.value = v.default))
+
+const clicks = computed(() => props.clicks)
+const navClicks = useNavClicks(clicks, props.nav.currentRoute, props.nav.currentPage)
+const id = computed(() => `${props.route.path.toString().padStart(3, '0')}-${(clicks.value + 1).toString().padStart(2, '0')}`)
+
+provide(injectionEnigmaSlidevContext, reactive({
+  nav: { ...props.nav, ...navClicks },
+  configs,
+  themeConfigs: computed(() => configs.themeConfig),
+}))
+</script>
+
+<template>
+  <div :id="id" class="print-slide-container" :style="style">
+    <GlobalBottom />
+
+    <SlideWrapper
+      :is="route?.component!"
+      v-model:clicks-elements="clicksElements"
+      :clicks="isClicksDisabled ? undefined : clicks"
+      :clicks-disabled="isClicksDisabled"
+      :class="getSlideClass(route)"
+      :route="route"
+    />
+    <template
+      v-if="
+        (__ENIGMASLIDEV_FEATURE_DRAWINGS__
+          || __ENIGMASLIDEV_FEATURE_DRAWINGS_PERSIST__)
+          && DrawingPreview
+      "
+    >
+      <DrawingPreview :page="+route.path" />
+    </template>
+
+    <GlobalTop />
+  </div>
+</template>
