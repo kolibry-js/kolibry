d53f64c52f98e6b6d3f748cf8d4568e20c2bea13
nyxb <contact@nyxb.email>
2023-11-25 14:45:12 +0100

    ✨ feat(client): add env.ts for EnigmaSlidev configuration handling
    This commit introduces a new `env.ts` file within the client package, which is responsible for setting up and exporting configuration values for the EnigmaSlidev presentation framework. It includes computed theme variables and slide dimensions based on the provided configuration, ensuring that the aspect ratio is honored and preventing additional empty pages during printing. This change allows for a more dynamic and configurable setup for client presentations.

---
+++ packages/client/env.ts
@@ -0,0 +1,25 @@
+import type { EnigmaSlidevConfig } from '@enigmaslidev/types'
+import type { UnwrapNestedRefs } from 'vue'
+import { computed } from 'vue'
+import { objectMap } from '@antfu/utils'
+
+// @ts-expect-error missing types
+import _configs from '/@enigmaslidev/configs'
+import type { EnigmaSlidevContext } from './modules/context'
+
+export const configs = _configs as EnigmaSlidevConfig
+export const slideAspect = configs.aspectRatio ?? (16 / 9)
+export const slideWidth = configs.canvasWidth ?? 980
+// To honor the aspect ratio more as possible, we need to approximate the height to the next integer.
+// Doing this, we will prevent on print, to create an additional empty white page after each page.
+export const slideHeight = Math.ceil(slideWidth / slideAspect)
+
+export const themeVars = computed(() => {
+  return objectMap(configs.themeConfig || {}, (k, v) => [`--enigmaslidev-theme-${k}`, v])
+})
+
+declare module 'vue' {
+  interface ComponentCustomProperties {
+    $enigmaslidev: UnwrapNestedRefs<EnigmaSlidevContext>
+  }
+}
