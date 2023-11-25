c83e4dcce66b22ddcc56c337145489e40598cb40
nyxb <contact@nyxb.email>
2023-11-25 16:08:23 +0100

    ✨ feat(NoteDisplay.vue): add new NoteDisplay component for rendering notes
    This component supports displaying notes either as raw text or as HTML, and includes a placeholder state for when no note content is available. It also emits a 'click' event to allow parent components to respond to user interactions.

---
+++ packages/client/internals/NoteDisplay.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+const props = defineProps<{
+  class?: string
+  noteHtml?: string
+  note?: string
+  placeholder?: string
+}>()
+
+defineEmits(['click'])
+</script>
+
+<template>
+  <div
+    v-if="noteHtml"
+    class="prose overflow-auto outline-none"
+    :class="props.class"
+    @click="$emit('click')"
+    v-html="noteHtml"
+  />
+  <div
+    v-else-if="note"
+    class="prose overflow-auto outline-none"
+    :class="props.class"
+    @click="$emit('click')"
+  >
+    <p v-text="note" />
+  </div>
+  <div
+    v-else
+    class="prose overflow-auto outline-none opacity-50 italic"
+    :class="props.class"
+    @click="$emit('click')"
+  >
+    <p v-text="props.placeholder || 'No notes.'" />
+  </div>
+</template>
