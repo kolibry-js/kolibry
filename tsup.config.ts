5ee9de909ed91cc9fd269c8bd3b789531efcee41
nyxb <contact@nyxb.email>
2023-11-25 01:42:37 +0100

    ✨ feat(tsup.config.ts): add tsup configuration for ESM format and Node 18 target
    This commit introduces a new `tsup.config.ts` file, which is used to configure the tsup build tool for the project. The configuration specifies that the output format should be ESM (ECMAScript modules), targets Node.js version 18, enables code splitting, generates declaration files (dts), cleans the output directory before building, disables shims, and marks packages under `@enigmaslidev` namespace as external to prevent them from being bundled.

---
+++ tsup.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  format: [
+    'esm',
+  ],
+  target: 'node18',
+  splitting: true,
+  dts: true,
+  clean: true,
+  shims: false,
+  external: [
+    /@enigmaslidev/,
+  ],
+})
