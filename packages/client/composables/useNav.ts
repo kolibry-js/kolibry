829163de354b85f528007e36a8b2e89ac762d626
nyxb <contact@nyxb.email>
2023-11-25 15:05:33 +0100

    ✨ feat(client/composables): add useNav composables for navigation logic

---
+++ packages/client/composables/useNav.ts
@@ -0,0 +1,52 @@
+import type { ComputedRef } from 'vue'
+import { computed } from 'vue'
+import type { RouteLocationNormalizedLoaded, RouteRecordRaw } from 'vue-router'
+import type { TocItem } from '@enigmaslidev/types'
+import type { EnigmaSlidevContextNav } from '../modules/context'
+import { addToTree, downloadPDF, filterTree, getPath, getTreeWithActiveStatuses, go, next, nextSlide, openInEditor, prev, prevSlide } from '../logic/nav'
+import { rawRoutes } from '../routes'
+
+export function useNav(route: ComputedRef<RouteRecordRaw | RouteLocationNormalizedLoaded>): EnigmaSlidevContextNav {
+  const path = computed(() => route.value.path)
+  const total = computed(() => rawRoutes.length)
+
+  const currentPage = computed(() => Number.parseInt(path.value.split(/\//g).slice(-1)[0]) || 1)
+  const currentPath = computed(() => getPath(currentPage.value))
+  const currentRoute = computed(() => rawRoutes.find(i => i.path === `${currentPage.value}`))
+  const currentSlideId = computed(() => currentRoute.value?.meta?.slide?.id)
+  const currentLayout = computed(() => currentRoute.value?.meta?.layout || (currentPage.value === 1 ? 'cover' : 'default'))
+
+  const nextRoute = computed(() => rawRoutes.find(i => i.path === `${Math.min(rawRoutes.length, currentPage.value + 1)}`))
+
+  const rawTree = computed(() => rawRoutes
+    .filter((route: RouteRecordRaw) => route.meta?.slide?.title)
+    .reduce((acc: TocItem[], route: RouteRecordRaw) => {
+      addToTree(acc, route)
+      return acc
+    }, []))
+  const treeWithActiveStatuses = computed(() => getTreeWithActiveStatuses(rawTree.value, currentRoute.value))
+  const tree = computed(() => filterTree(treeWithActiveStatuses.value))
+
+  return {
+    rawRoutes,
+    route,
+    path,
+    total,
+    currentPage,
+    currentPath,
+    currentRoute,
+    currentSlideId,
+    currentLayout,
+    nextRoute,
+    rawTree,
+    treeWithActiveStatuses,
+    tree,
+    go,
+    downloadPDF,
+    next,
+    nextSlide,
+    openInEditor,
+    prev,
+    prevSlide,
+  }
+}
