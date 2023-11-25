9c81a0213602c511a5ff4fd36a9dcd02dc770823
nyxb <contact@nyxb.email>
2023-11-25 15:03:28 +0100

    ✨ feat(client): add VClicks component for click animations
    This commit introduces a new Vue component, VClicks, which provides click animations for list items. The component supports various properties to control the depth, frequency, and behavior of the animations, such as hiding or fading elements. It also includes logic to handle nested lists and apply the appropriate directives based on the specified properties. This feature enhances the user interface by providing visual feedback on click interactions, making the application more interactive and engaging.

---
+++ packages/client/builtin/VClicks.ts
@@ -0,0 +1,123 @@
+/**
+ * <v-clicks/> click animations component
+ *
+ * Learn more: https://sli.dev/guide/animations.html#click-animations
+ */
+
+import { toArray } from '@nyxb/utils'
+import type { Directive, VNode, VNodeArrayChildren } from 'vue'
+
+import { Comment, defineComponent, h, isVNode, resolveDirective, withDirectives } from 'vue'
+
+const listTags = ['ul', 'ol']
+
+export default defineComponent({
+  props: {
+    depth: {
+      type: [Number, String],
+      default: 1,
+    },
+    every: {
+      type: Number,
+      default: 1,
+    },
+    at: {
+      type: [Number, String],
+      default: null,
+    },
+    hide: {
+      type: Boolean,
+      default: false,
+    },
+    fade: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  render() {
+    const click = resolveDirective('click')!
+    const after = resolveDirective('after')!
+
+    const applyDirective = (node: VNode, directive: Directive, delta: number | string) => {
+      return withDirectives(node, [[
+        directive,
+        delta,
+        '',
+        {
+          hide: this.hide,
+          fade: this.fade,
+        },
+      ]])
+    }
+
+    const openAllTopLevelSlots = <T extends VNodeArrayChildren | VNode[]>(children: T): T => {
+      return children.flatMap((i) => {
+        if (isVNode(i) && typeof i.type === 'symbol' && Array.isArray(i.children))
+          return openAllTopLevelSlots(i.children)
+        else
+          return [i]
+      }) as T
+    }
+
+    let defaults = this.$slots.default?.()
+
+    if (!defaults)
+      return
+
+    defaults = openAllTopLevelSlots(toArray(defaults))
+
+    const mapSubList = (children: VNodeArrayChildren, depth = 1): [VNodeArrayChildren, number] => {
+      let idx = 0
+      const vNodes = openAllTopLevelSlots(children).map((i) => {
+        if (!isVNode(i))
+          return i
+        if (listTags.includes(i.type as string) && Array.isArray(i.children)) {
+          // eslint-disable-next-line ts/no-use-before-define
+          const [vNodes, total] = mapChildren(i.children, depth + 1)
+          idx += total
+          return h(i, {}, [vNodes])
+        }
+        return h(i)
+      })
+      return [vNodes, idx]
+    }
+
+    let globalIdx = 0
+    const mapChildren = (children: VNodeArrayChildren, depth = 1): [VNodeArrayChildren, number] => {
+      let idx = 0
+      const vNodes = openAllTopLevelSlots(children).map((i) => {
+        if (!isVNode(i) || i.type === Comment)
+          return i
+        const directive = idx % this.every === 0 ? click : after
+        let vNode
+        let childCount = 0
+        if (depth < +this.depth && Array.isArray(i.children)) {
+          const [vNodes, total] = mapSubList(i.children, depth)
+          vNode = h(i, {}, [vNodes])
+          childCount = total
+          idx += total + 1
+        }
+        else {
+          vNode = h(i)
+          idx++
+        }
+        const delta = this.at != null
+          ? Number(this.at) + Math.floor(globalIdx / this.every) + depth
+          : (depth - 1 - childCount).toString()
+        globalIdx++
+        return applyDirective(
+          vNode,
+          directive,
+          (typeof delta === 'string' && !delta.startsWith('-')) ? `+${delta}` : delta,
+        )
+      })
+      return [vNodes, idx]
+    }
+
+    // handle ul, ol list
+    if (defaults.length === 1 && listTags.includes(defaults[0].type as string) && Array.isArray(defaults[0].children))
+      return h(defaults[0], {}, [mapChildren(defaults[0].children)[0]])
+
+    return mapChildren(defaults)[0]
+  },
+})
