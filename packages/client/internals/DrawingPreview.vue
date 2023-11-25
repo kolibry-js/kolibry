6e1807e40142ece74c79d3a04dc7fa0f5effa08f
nyxb <contact@nyxb.email>
2023-11-25 16:07:18 +0100

    ✨ feat(DrawingPreview.vue): add new DrawingPreview component for rendering SVG drawings
    This new Vue component is responsible for rendering SVG drawings based on the current page state. It uses the `drawingState` reactive property to conditionally display the SVG content. The component is designed to be full width and height, positioned absolutely at the top, and does not interfere with pointer events, making it suitable for overlaying on other UI elements.

---
+++ packages/client/internals/DrawingPreview.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { drawingState } from '../logic/drawings'
+
+defineProps<{ page: number }>()
+</script>
+
+<template>
+  <svg
+    v-if="drawingState[page]"
+    class="w-full h-full absolute top-0 pointer-events-none"
+    v-html="drawingState[page]"
+  />
+</template>
