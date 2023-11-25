d7002652fff07005dd9f84d6f5be871f7a5eacb8
nyxb <contact@nyxb.email>
2023-11-25 16:38:19 +0100

    ✨ feat(enigmaslides): add options configuration for EnigmaSlidev package
    This commit introduces a new configuration file for the EnigmaSlidev package, which includes a variety of options for setting up the slide presentation environment. The options.ts file provides a structured way to configure themes, remote access, and various plugins such as Vue, VueJsx, Icons, Components, Markdown, WindiCSS, UnoCSS, RemoteAssets, and ServerRef. It also includes utility functions for resolving paths and loading theme metadata, which are essential for customizing and enhancing the presentation experience. This addition lays the groundwork for a more flexible and customizable slide presentation setup.

---
+++ packages/enigmaslides/node/options.ts
@@ -0,0 +1,191 @@
+import { dirname, join, resolve } from 'node:path'
+import process from 'node:process'
+import { fileURLToPath } from 'node:url'
+import type Vue from '@vitejs/plugin-vue'
+import type VueJsx from '@vitejs/plugin-vue-jsx'
+import type Icons from 'unplugin-icons/vite'
+import type Components from 'unplugin-vue-components/vite'
+import type Markdown from 'unplugin-vue-markdown/vite'
+import type WindiCSS from 'vite-plugin-windicss'
+import type { VitePluginConfig as UnoCSSConfig } from 'unocss/vite'
+import type RemoteAssets from 'vite-plugin-remote-assets'
+import type ServerRef from 'vite-plugin-vue-server-ref'
+import type { ArgumentsType } from '@nyxb/utils'
+import { uniq } from '@nyxb/utils'
+import type { EnigmaSlidevMarkdown } from '@enigmaslidev/types'
+import _debug from 'debug'
+import { parser } from './parser'
+import { packageExists, resolveImportPath } from './utils'
+import { getThemeMeta, promptForThemeInstallation, resolveThemeName } from './themes'
+import { getAddons } from './addons'
+
+const debug = _debug('enigmaslidev:options')
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export interface EnigmaSlidevEntryOptions {
+  /**
+   * Markdown entry
+   *
+   * @default 'slides.md'
+   */
+  entry?: string
+
+  /**
+   * Theme id
+   */
+  theme?: string
+
+  /**
+   * Remote password
+   */
+  remote?: string
+
+  /**
+   * Root path
+   *
+   * @default process.cwd()
+   */
+  userRoot?: string
+
+  /**
+   * Enable inspect plugin
+   */
+  inspect?: boolean
+}
+
+export interface ResolvedEnigmaSlidevOptions {
+  data: EnigmaSlidevMarkdown
+  entry: string
+  userRoot: string
+  cliRoot: string
+  clientRoot: string
+  theme: string
+  themeRoots: string[]
+  addonRoots: string[]
+  roots: string[]
+  mode: 'dev' | 'build' | 'export'
+  remote?: string
+  inspect?: boolean
+}
+
+export interface EnigmaSlidevPluginOptions extends EnigmaSlidevEntryOptions {
+  vue?: ArgumentsType<typeof Vue>[0]
+  vuejsx?: ArgumentsType<typeof VueJsx>[0]
+  markdown?: ArgumentsType<typeof Markdown>[0]
+  components?: ArgumentsType<typeof Components>[0]
+  windicss?: ArgumentsType<typeof WindiCSS>[0]
+  icons?: ArgumentsType<typeof Icons>[0]
+  remoteAssets?: ArgumentsType<typeof RemoteAssets>[0]
+  serverRef?: ArgumentsType<typeof ServerRef>[0]
+  unocss?: UnoCSSConfig
+}
+
+export interface EnigmaSlidevServerOptions {
+  onDataReload?: (newData: EnigmaSlidevMarkdown, data: EnigmaSlidevMarkdown) => void
+}
+
+export function getClientRoot() {
+  return dirname(resolveImportPath('@enigmaslidev/client/package.json', true))
+}
+
+export function getCLIRoot() {
+  return resolve(__dirname, '..')
+}
+
+export function isPath(name: string) {
+  return name.startsWith('/') || /^\.\.?[\/\\]/.test(name)
+}
+
+export function getThemeRoots(name: string, entry: string) {
+  if (!name)
+    return []
+
+  // TODO: handle theme inherit
+  return [getRoot(name, entry)]
+}
+
+export function getAddonRoots(addons: string[], entry: string) {
+  if (addons.length === 0)
+    return []
+  return addons.map(name => getRoot(name, entry))
+}
+
+export function getRoot(name: string, entry: string) {
+  if (isPath(name))
+    return resolve(dirname(entry), name)
+  return dirname(resolveImportPath(`${name}/package.json`, true))
+}
+
+export function getUserRoot(options: EnigmaSlidevEntryOptions) {
+  const { entry: rawEntry = 'slides.md', userRoot = process.cwd() } = options
+  const fullEntry = resolve(userRoot, rawEntry)
+  return { entry: fullEntry, userRoot: dirname(fullEntry) }
+}
+
+export async function resolveOptions(
+  options: EnigmaSlidevEntryOptions,
+  mode: ResolvedEnigmaSlidevOptions['mode'],
+  promptForInstallation = true,
+): Promise<ResolvedEnigmaSlidevOptions> {
+  const { remote, inspect } = options
+  const {
+    entry,
+    userRoot,
+  } = getUserRoot(options)
+  const data = await parser.load(entry)
+  const theme = resolveThemeName(options.theme || data.config.theme)
+
+  if (promptForInstallation) {
+    if (await promptForThemeInstallation(theme) === false)
+      process.exit(1)
+  }
+  else {
+    if (!packageExists(theme)) {
+      console.error(`Theme "${theme}" not found, have you installed it?`)
+      process.exit(1)
+    }
+  }
+
+  const clientRoot = getClientRoot()
+  const cliRoot = getCLIRoot()
+  const themeRoots = getThemeRoots(theme, entry)
+  const addons = await getAddons(userRoot, data.config)
+  const addonRoots = getAddonRoots(addons, entry)
+  const roots = uniq([clientRoot, ...themeRoots, ...addonRoots, userRoot])
+
+  if (themeRoots.length) {
+    const themeMeta = await getThemeMeta(theme, join(themeRoots[0], 'package.json'))
+    data.themeMeta = themeMeta
+    if (themeMeta)
+      data.config = parser.resolveConfig(data.headmatter, themeMeta, options.entry)
+  }
+
+  debug({
+    config: data.config,
+    mode,
+    entry,
+    theme,
+    userRoot,
+    clientRoot,
+    cliRoot,
+    themeRoots,
+    addonRoots,
+    roots,
+    remote,
+  })
+
+  return {
+    data,
+    mode,
+    entry,
+    theme,
+    userRoot,
+    clientRoot,
+    cliRoot,
+    themeRoots,
+    addonRoots,
+    roots,
+    remote,
+    inspect,
+  }
+}
