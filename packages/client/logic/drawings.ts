bed774d44976230038d58ac94eb04abac8e8e10c
nyxb <contact@nyxb.email>
2023-11-25 14:45:39 +0100

    ✨ feat(client/logic): add drawing logic for interactive canvas
    This commit introduces a new file `drawings.ts` within the client logic package, which encapsulates the functionality required for an interactive drawing feature on a canvas. The drawing logic includes brush color selection, undo/redo capabilities, canvas clearing, and drawing mode toggling (stylus, line, arrow, rectangle, ellipse). It also integrates with local storage to persist user preferences and leverages Vue's reactivity system to update the UI in response to state changes. The drawing feature is designed to be synchronized across users, with special considerations for presenter mode in collaborative environments. This enhancement aims to provide users with a more dynamic and engaging experience when interacting with the application's canvas elements.

---
+++ packages/client/logic/drawings.ts
@@ -0,0 +1,158 @@
+import { computed, markRaw, nextTick, reactive, ref, watch } from 'vue'
+import type { Brush, Options as DrauuItOptions, DrawingMode } from 'drauu-it'
+import { createDrauuIt } from 'drauu-it'
+import { toReactive, useLocalStorage } from '@vueuse/core'
+import { drawingState, onPatch, patch } from '../state/drawings'
+import { configs } from '../env'
+import { currentPage, isPresenter } from './nav'
+
+export const brushColors = [
+  '#ff595e',
+  '#ffca3a',
+  '#8ac926',
+  '#1982c4',
+  '#6a4c93',
+  '#ffffff',
+  '#000000',
+]
+
+export const drawingEnabled = useLocalStorage('enigmaslidev-drawing-enabled', false)
+export const drawingPinned = useLocalStorage('enigmaslidev-drawing-pinned', false)
+export const canUndo = ref(false)
+export const canRedo = ref(false)
+export const canClear = ref(false)
+export const isDrawing = ref(false)
+
+export const brush = toReactive(useLocalStorage<Brush>('enigmaslidev-drawing-brush', {
+  color: brushColors[0],
+  size: 4,
+  mode: 'stylus',
+}))
+
+const _mode = ref<DrawingMode | 'arrow'>('stylus')
+const syncUp = computed(() => configs.drawings.syncAll || isPresenter.value)
+let disableDump = false
+
+export const drawingMode = computed({
+  get() {
+    return _mode.value
+  },
+  set(v: DrawingMode | 'arrow') {
+    _mode.value = v
+    if (v === 'arrow') {
+      brush.mode = 'line'
+      brush.arrowEnd = true
+    }
+    else {
+      brush.mode = v
+      brush.arrowEnd = false
+    }
+  },
+})
+
+export const drauuItOptions: DrauuItOptions = reactive({
+  brush,
+  acceptsInputTypes: computed(() => (drawingEnabled.value && (!configs.drawings.presenterOnly || isPresenter.value)) ? undefined : ['pen' as const]),
+  coordinateTransform: false,
+})
+export const drauuIt = markRaw(createDrauuIt(drauuItOptions))
+
+export function clearDrauuIt() {
+  drauuIt.clear()
+  if (syncUp.value)
+    patch(currentPage.value, '')
+}
+
+export function updateState() {
+  canRedo.value = drauuIt.canRedo()
+  canUndo.value = drauuIt.canUndo()
+  canClear.value = !!drauuIt.el?.children.length
+}
+
+export function loadCanvas(page?: number) {
+  disableDump = true
+  const data = drawingState[page || currentPage.value]
+  if (data != null)
+    drauuIt.load(data)
+  else
+    drauuIt.clear()
+  updateState()
+  disableDump = false
+}
+
+drauuIt.on('changed', () => {
+  updateState()
+  if (!disableDump) {
+    const dump = drauuIt.dump()
+    const key = currentPage.value
+    if ((drawingState[key] || '') !== dump && syncUp.value)
+      patch(key, drauuIt.dump())
+  }
+})
+
+onPatch((state: any) => {
+  disableDump = true
+  if (state[currentPage.value] != null)
+    drauuIt.load(state[currentPage.value] || '')
+  disableDump = false
+  updateState()
+})
+
+nextTick(() => {
+  watch(currentPage, () => {
+    if (!drauuIt.mounted)
+      return
+    loadCanvas()
+  }, { immediate: true })
+})
+
+drauuIt.on('start', () => isDrawing.value = true)
+drauuIt.on('end', () => isDrawing.value = false)
+
+window.addEventListener('keydown', (e) => {
+  if (!drawingEnabled.value)
+    return
+
+  const noModifier = !e.ctrlKey && !e.altKey && !e.shiftKey && !e.metaKey
+  let handled = true
+  if (e.code === 'KeyZ' && (e.ctrlKey || e.metaKey)) {
+    if (e.shiftKey)
+      drauuIt.redo()
+    else
+      drauuIt.undo()
+  }
+  else if (e.code === 'Escape') {
+    drawingEnabled.value = false
+  }
+  else if (e.code === 'KeyL' && noModifier) {
+    drawingMode.value = 'line'
+  }
+  else if (e.code === 'KeyA' && noModifier) {
+    drawingMode.value = 'arrow'
+  }
+  else if (e.code === 'KeyS' && noModifier) {
+    drawingMode.value = 'stylus'
+  }
+  else if (e.code === 'KeyR' && noModifier) {
+    drawingMode.value = 'rectangle'
+  }
+  else if (e.code === 'KeyE' && noModifier) {
+    drawingMode.value = 'ellipse'
+  }
+  else if (e.code === 'KeyC' && noModifier) {
+    clearDrauuIt()
+  }
+  else if (e.code.startsWith('Digit') && noModifier && +e.code[5] <= brushColors.length) {
+    brush.color = brushColors[+e.code[5] - 1]
+  }
+  else {
+    handled = false
+  }
+
+  if (handled) {
+    e.preventDefault()
+    e.stopPropagation()
+  }
+}, false)
+
+export { drawingState }
