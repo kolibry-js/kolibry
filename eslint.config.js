e1a9cc45dea49c9ec41077cf3d997782e22bb885
nyxb <contact@nyxb.email>
2023-11-26 17:52:40 +0100

    🎨 style(eslint.config.js): disable specific Vue linting rules for better DX
    🛠️ build(netlify.toml): switch build command from bun to pnpm for consistency
    
    🩹 fix(package.json): update various dependencies and scripts for project maintenance
    
    ♻️ refactor(tsconfig.json): remove trailing whitespace for cleaner file format

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,12 @@
 import nyxb from '@nyxb/eslint-config'
 
-export default nyxb({
-  rules: {
-    'ts/no-use-before-define': 0,
+export default nyxb(
+  {
+    overrides: {
+      vue: {
+        'vue/no-v-text-v-html-on-component': 'off',
+        'vue/component-name-in-template-casing': 'off',
+      },
+    },
   },
-})
+)
