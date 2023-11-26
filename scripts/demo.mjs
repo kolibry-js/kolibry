39aaa67a9728e312bacf10a5b98bcd0cd5d8f926
nyxb <contact@nyxb.email>
2023-11-26 17:48:39 +0100

    ✨ feat(scripts): add demo, publish, and update-versions scripts
    - `demo.mjs` script for building demo projects with kolibri
    - `publish.mjs` script to copy README and publish packages
    - `update-versions.mjs` to update package versions in templates

---
+++ scripts/demo.mjs
@@ -0,0 +1,16 @@
+import { fileURLToPath } from 'node:url'
+import { $, cd, fs, path } from 'zx'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+await $`npm run build`
+
+const starterMd = path.resolve(__dirname, '../demo/starter/slides.md')
+if (!fs.existsSync(starterMd))
+  await fs.copyFile(path.resolve(__dirname, '../packages/create-app/template/slides.md'), starterMd)
+
+cd(path.resolve(__dirname, '../demo/composable-vue'))
+await $`npx kolibri build -d --base /composable-vue/ --out ../../dist/composable-vue`
+cd(path.resolve(__dirname, '../demo/starter'))
+await $`npx kolibri build -d --base /starter/ --out ../../dist/starter`
---
+++ scripts/update-versions.mjs
@@ -0,0 +1,26 @@
+import { join } from 'node:path'
+import { objectMap } from '@nyxb/utils'
+import { fs } from 'zx'
+
+const templates = [
+  'packages/create-app/template',
+  'packages/create-theme/template',
+]
+
+const { version } = await fs.readJSON('package.json')
+
+for (const template of templates) {
+  const path = join(template, 'package.json')
+  const pkg = await fs.readJSON(path)
+  const deps = ['dependencies', 'devDependencies']
+  for (const name of deps) {
+    if (!pkg[name])
+      continue
+    pkg[name] = objectMap(pkg[name], (k, v) => {
+      if (k.startsWith('@kolibrijs/') && !k.startsWith('@kolibrijs/theme'))
+        return [k, `^${version}`]
+      return [k, v]
+    })
+  }
+  await fs.writeJSON(path, pkg, { spaces: 2 })
+}
