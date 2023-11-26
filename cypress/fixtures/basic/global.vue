d4e4acd1f301e9f5556f983444340dd0c81467cd
nyxb <contact@nyxb.email>
2023-11-26 17:46:53 +0100

    ✨ feat(cypress/fixtures): add global footer Vue component for testing
    This component will only appear on odd-numbered pages, aiding in the testing of conditional rendering within the application.

---
+++ cypress/fixtures/basic/global.vue
@@ -0,0 +1,8 @@
+<template>
+  <div
+    v-if="$kolibri.nav.currentPage % 2 === 1"
+    class="absolute bottom-0 right-0 m-10 hover:text-red-400"
+  >
+    Global Footer (appear only on odd page)
+  </div>
+</template>
